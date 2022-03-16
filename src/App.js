dd6217166027680a0d6ed63a3ac00b328633928d
Luka Trkulja <luka97zr@gmail.com>
2022-03-17 00:06:33 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -1,47 +1,53 @@
+import { filter } from "minimatch";
+import React, {useState} from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/Expenses/NewExpense";
 
-function App() {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-    {
-      id: 'e5',
-      title: 'New Desk (Metal)',
-      amount: 250,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+const INITIAL_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+  {
+    id: 'e5',
+    title: 'New Desk (Metal)',
+    amount: 250,
+    date: new Date(2021, 5, 12),
+  },
+ 
+];
 
-  const addExpenseHandler = (expense) =>{
-    console.log('App.js');
-    console.log(expense);
-    expenses.push(expense);
-  }
+
+function App() {
+  const [expenseState,setExpenseState] = useState(INITIAL_EXPENSES);
 
   
+  const addExpenseHandler = (expense) =>{
+    setExpenseState((prevExpenses=>{
+      return [expense,...prevExpenses];
+    }))
+  }
+
 
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler}/>
-      <Expenses items={expenses}/>
+      <Expenses items={expenseState}/>
     </div>
   );
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -10,19 +10,14 @@ function Expenses(props){
             console.log(`test ${year}`);
             setFilteredYear(year);
         }
-    // return(
-    //     <div className="expenses">
-    //         <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}/>
-    //         <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date}/>
-    //         <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date}/>
-    //         <ExpenseItem title={props.items[3].title} amount={props.items[3].amount} date={props.items[3].date}/>
-    //     </div>
-    // )
+        const filteredExpenses = props.items.filter(el => {
+            return el.date.getFullYear().toString() === filteredYear;
+        })
     return(
         <div>
         <Card className="expenses">
             <ExpensesFilter  value={filteredYear} onFilterChange={filterChangeHandler}/>
-            {props.items.map(el=>
+            {filteredExpenses.map(el=>
                 <ExpenseItem key={el.id} title={el.title} amount={el.amount} date={el.date}/>
             )}
         </Card>
